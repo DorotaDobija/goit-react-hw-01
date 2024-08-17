96c91e605219bd975080498aebdc9135fa4cda24
Dorota Dobija <117933735+DorotaDobija@users.noreply.github.com>
2024-08-17 13:33:23 +0200

    third task done

---
+++ goit-react-hw-01/src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,25 @@
+import css from './transactionHistory.module.css'
+
+export const TransactionHistory = ({ items }) => {
+    return (
+    <table className={css.table}>
+  <thead>
+    <tr className={css.heading_row}>
+      <th>Type</th>
+      <th>Amount</th>
+      <th>Currency</th>
+    </tr>
+  </thead>
+        <tbody>
+        {items.map((item, i) => (
+            <tr className={
+                i%2 !== 0 && css.grey_row
+            } key={item.id}>
+                <td className={css.transaction_type}>{item.type}</td>
+                <td>{item.amount}</td>
+                <td>{item.currency}</td>
+            </tr>
+                    ))}
+  </tbody>
+</table>
+)}
